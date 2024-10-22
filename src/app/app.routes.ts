2a4ef6db147289dfdb958fb92ea8d733b823c5a3
EmilianoJGit98 <ejg250398@gmail.com>
2024-10-22 19:53:43 -0300

    inicio gestion eventos municipales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { ActividadesComponent } from './actividades/actividades.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent,
+    children:  [
+      { path: '', component: EventosComponent},
+      { path: 'eventos', component: EventosComponent},
+      { path: 'actividades', component: ActividadesComponent}
+    ],
+   },
+];
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [HeaderComponent, RouterModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
