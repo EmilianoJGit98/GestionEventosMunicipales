1959874672a4f2d0f1f74dd45c814a34ade685bf
EmilianoJGit98 <ejg250398@gmail.com>
2024-10-28 08:43:15 -0300

    modal alta evento

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventoInterface } from '../Models/eventos.model';
 // import { EventosDisplayService } from '../services/gestion-eventos.service';
 import { CommonModule } from '@angular/common';
@@ -9,28 +9,67 @@ import { RouterModule } from '@angular/router';
 import { Decodebase64Pipe } from '../pipes/decodebase64.pipe';
 import { EventosMunicipalesService } from '../services/eventos-municipales.service';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AddEventoComponent } from './add-evento/add-evento.component';
+import { MatDialog } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 
 
 @Component({
   selector: 'app-eventos',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatMenuModule, MatButtonModule, RouterModule, Decodebase64Pipe],
-  templateUrl: './eventos.component.html',
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    RouterModule,
+    Decodebase64Pipe,
+    ReactiveFormsModule
+  ],
+      templateUrl: './eventos.component.html',
   styleUrl: './eventos.component.css',
 })
 export class EventosComponent {
+  formAltaEvento: FormGroup;
   eventos: EventoInterface[] = [];
 
   constructor(
-    private servEventos: EventosMunicipalesService
+    private servEventos: EventosMunicipalesService,
+    private BuildForm: FormBuilder
+    // private addModal: MatDialog,
     // private getListEventos: EventosDisplayService
 
-  ) {}
+  ) {
+
+    this.formAltaEvento = this.BuildForm.group({
+      nombre: [
+        '',
+        [
+          Validators.required,
+        ],
+      ],
+      fechaDesde: [
+        '',
+        [
+          Validators.required,
+        ],
+      ],
+      fechaHasta: [
+        '',
+        [
+          Validators.required,
+        ],
+      ],
+    });
+
+  }
 
   ngOnInit() {
     this.cargarEventos();
     // this.eventos = this.getListEventos.ListarEventos();
+
   }
 
   cargarEventos(): void {
@@ -44,4 +83,17 @@ export class EventosComponent {
       }
     );
   }
+
+
+  onSubmit() {
+    if (this.formAltaEvento.valid) {
+      // const formData = this.registroForm.value;
+      // this.dataRegistro.updateFormData(formData); //actualizar los datos de la persona en el servicio
+
+      // this.router.navigate(['/actividades'], {
+      //   queryParams: { IDevento: this.IDevento },
+      // });
+    }
+  }
+
 }
---
+++ src/app/eventos/up-evento/up-evento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-up-evento',
+  standalone: true,
+  imports: [],
+  templateUrl: './up-evento.component.html',
+  styleUrl: './up-evento.component.css'
+})
+export class UpEventoComponent {
+
+}
