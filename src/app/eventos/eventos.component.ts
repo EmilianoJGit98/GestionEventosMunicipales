bacaa7f4533faeef6d65b3393551b7c01e4718c3
Andres Martinez <139396923+Andresfm7@users.noreply.github.com>
2024-10-29 11:07:32 -0300

    modal alta evento, vista previa de imagen al cargar, validaciones (falta enviar)

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -11,10 +11,12 @@ import { EventosMunicipalesService } from '../services/eventos-municipales.servi
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AddEventoComponent } from './add-evento/add-evento.component';
 import { MatDialog } from '@angular/material/dialog';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
-
-
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-eventos',
@@ -26,50 +28,35 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
     MatButtonModule,
     RouterModule,
     Decodebase64Pipe,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
-      templateUrl: './eventos.component.html',
+  templateUrl: './eventos.component.html',
   styleUrl: './eventos.component.css',
 })
 export class EventosComponent {
   formAltaEvento: FormGroup;
   eventos: EventoInterface[] = [];
+  archivoCargado: boolean = false;
+  previsualizacion: string | ArrayBuffer | null = null;
+
+  iconEmpty = '<i class="fas fas fa-cloud-upload-alt fs-alto mt-2"></i>';
+  iconNoEmpty = '<i class="fas fas fa-check-circle fs-alto mt-2"></i>';
 
   constructor(
     private servEventos: EventosMunicipalesService,
-    private BuildForm: FormBuilder
-    // private addModal: MatDialog,
-    // private getListEventos: EventosDisplayService
-
+    private BuildForm: FormBuilder // private addModal: MatDialog, // private getListEventos: EventosDisplayService
   ) {
-
     this.formAltaEvento = this.BuildForm.group({
-      nombre: [
-        '',
-        [
-          Validators.required,
-        ],
-      ],
-      fechaDesde: [
-        '',
-        [
-          Validators.required,
-        ],
-      ],
-      fechaHasta: [
-        '',
-        [
-          Validators.required,
-        ],
-      ],
+      nombre: ['', [Validators.required]],
+      fechaDesde: ['', [Validators.required]],
+      fechaHasta: ['', [Validators.required]],
+      imagen: ['', [Validators.required]],
     });
-
   }
 
   ngOnInit() {
     this.cargarEventos();
     // this.eventos = this.getListEventos.ListarEventos();
-
   }
 
   cargarEventos(): void {
@@ -84,16 +71,42 @@ export class EventosComponent {
     );
   }
 
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.archivoCargado = true;
+      const fileName = file.name;
+      const filePath = file.webkitRelativePath || file.name; // Para obtener la ruta (nombre del archivo), ya que el navegador no te da el path
+      //console.log('Nombre del archivo:', fileName);
+      //console.log('Ruta del archivo:', filePath);
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const base64String = e.target.result; // Esto es el base64
+        this.previsualizacion = e.target.result;
+        //console.log('Base64:', base64String);
+      };
+      reader.readAsDataURL(file); // Leer el archivo y convertirlo a Base64
+    }
+  }
+
+  resetFile() {
+    this.formAltaEvento.get('imagen')?.setValue(''); // Limpia el campo del input
+    this.archivoCargado = false; // Cambia el estado a falso
+    this.previsualizacion = null; // Resetea la vista previa
+    const fileInput: any = document.getElementById('file'); // Obtiene el elemento del input
+    if (fileInput) {
+      fileInput.value = ''; // Limpia el valor del input de archivo
+    }
+  }
 
   onSubmit() {
     if (this.formAltaEvento.valid) {
       // const formData = this.registroForm.value;
       // this.dataRegistro.updateFormData(formData); //actualizar los datos de la persona en el servicio
-
       // this.router.navigate(['/actividades'], {
       //   queryParams: { IDevento: this.IDevento },
       // });
     }
   }
-
 }
